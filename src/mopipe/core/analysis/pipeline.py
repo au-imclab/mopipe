97a57f5837dec939f590c501004b9accc8165dcf
zeyus <zeyus@zeyus.com>
2024-03-07 11:10:05 +0100

    fixeds input to be x (was inconsistent), added pipeline test.

--- src/mopipe/core/analysis/pipeline.py
+++ src/mopipe/core/analysis/pipeline.py
@@ -28,7 +28,7 @@ class Pipeline(t.MutableSequence[Segment]):
 
     def _check_kwargs(self, **kwargs) -> None:
         """Check the arguments for the pipeline."""
-        if "input" not in kwargs:
+        if "x" not in kwargs:
             msg = "No input provided to pipeline."
             raise ValueError(msg)
 
@@ -43,14 +43,13 @@ class Pipeline(t.MutableSequence[Segment]):
 
     def run(self, **kwargs) -> t.Any:
         """Run the pipeline."""
-        output = None
         self._check_kwargs(**kwargs)
         for segment in self._segments:
             # most basic version here
             # we could also keep track of the output from each step
             # if that is useful, for now it's just I -> Segment -> O -> Segment -> O -> ...
             kwargs["x"] = segment(**kwargs)
-        return output
+        return kwargs["x"]
 
     def __repr__(self) -> str:
         return f"Pipeline(segments={self._segments})"
@@ -75,3 +74,37 @@ class Pipeline(t.MutableSequence[Segment]):
 
     def __contains__(self, value: object) -> bool:
         return value in self._segments
+
+    @t.overload
+    def __setitem__(self, index: int, value: Segment) -> None: ...
+
+    @t.overload
+    def __setitem__(self, index: slice, value: t.Iterable[Segment]) -> None: ...
+
+    def __setitem__(self, index: t.Union[int, slice], value: t.Union[Segment, t.Iterable[Segment]]) -> None:
+        if isinstance(index, int):
+            if not isinstance(value, Segment):
+                msg = "Single value must be a Segment."
+                raise ValueError(msg)
+            self._segments[index] = value
+        else:
+            if not isinstance(value, t.Iterable):
+                msg = "Value must be an iterable of Segments."
+                raise ValueError(msg)
+            if not all(isinstance(v, Segment) for v in value):
+                msg = "All values must be Segments."
+                raise ValueError(msg)
+            self._segments[index] = list(value)
+
+
+    @t.overload
+    def __delitem__(self, index: int) -> None: ...
+
+    @t.overload
+    def __delitem__(self, index: slice) -> None: ...
+
+    def __delitem__(self, index: t.Union[int, slice]) -> None:
+        del self._segments[index]
+
+    def insert(self, index: int, value: Segment) -> None:
+        self._segments.insert(index, value)
